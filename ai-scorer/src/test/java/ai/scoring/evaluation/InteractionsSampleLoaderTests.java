259ea35fb2dc47a1a196b2bcc2efbe2b2aabb937
Eric Deandrea <eric.deandrea@gmail.com>
2026-01-28 09:46:38 -0500

    Scoring work

--- ai-scorer/src/test/java/ai/scoring/evaluation/InteractionsSampleLoaderTests.java
+++ ai-scorer/src/test/java/ai/scoring/evaluation/InteractionsSampleLoaderTests.java
@@ -1,6 +1,7 @@
 package ai.scoring.evaluation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -12,7 +13,11 @@ import org.junit.jupiter.api.Test;
 import ai.scoring.domain.interaction.Interaction;
 import ai.scoring.domain.sample.Source;
 import ai.scoring.repository.InteractionRepository;
+import io.quarkiverse.langchain4j.testing.evaluation.EvaluationSample;
+import io.quarkiverse.langchain4j.testing.evaluation.Parameters;
+import io.quarkiverse.langchain4j.testing.evaluation.SampleLoadException;
 import io.quarkiverse.langchain4j.testing.evaluation.SampleLoaderResolver;
+import io.quarkiverse.langchain4j.testing.evaluation.Samples;
 
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
@@ -46,13 +51,13 @@ class InteractionsSampleLoaderTests {
 	}
 
 	@Test
-	void supportsWorksWhenNoSourceFound() {
+	void supportsWorksWhenNoInteractionFound() {
 		assertThat(this.loader.supports(Source.asSourceString(INTERACTION)))
 			.isFalse();
 	}
 
 	@Test
-	void supportsWorks() {
+	void supportsWorksWhenInteractionExists() {
 		loadSampleData();
 
 		assertThat(this.loader.supports(Source.asSourceString(INTERACTION)))
@@ -60,13 +65,39 @@ class InteractionsSampleLoaderTests {
 	}
 
 	@Test
-	void loaderLoadsWhenSourceExists() {
+	void loadWorksWhenInteractionExists() {
 		loadSampleData();
+		assertSample(this.loader.load(Source.asSourceString(INTERACTION), String.class));
+	}
 
-		var samples = SampleLoaderResolver.load(Source.asSourceString(INTERACTION), String.class);
+	@Test
+	void loaderLoadsWhenInteractionExists() {
+		loadSampleData();
+		assertSample(SampleLoaderResolver.load(Source.asSourceString(INTERACTION), String.class));
+	}
+
+	@Test
+	void loaderLoadsWhenInteractionDoesNotExist() {
+		assertThatExceptionOfType(SampleLoadException.class)
+			.isThrownBy(() -> SampleLoaderResolver.load(Source.asSourceString(INTERACTION), String.class))
+			.withMessageContaining("No SampleLoader found supporting source: %s", Source.asSourceString(INTERACTION));
+	}
+
+	private static void assertSample(Samples<String> samples) {
+		var expectedSample = EvaluationSample.<String>builder()
+			.withName(INTERACTION.getInteractionId().toString())
+			.withParameters(
+				new Parameters()
+					.add("systemMessage", INTERACTION.getSystemMessage())
+					.add("userMessage", INTERACTION.getUserMessage())
+			)
+			.withExpectedOutput(INTERACTION.getResult())
+      .build();
 
 		assertThat(samples)
-			.isNotNull();
+			.singleElement()
+			.usingRecursiveComparison()
+			.isEqualTo(expectedSample);
 	}
 
 	private void assertRepoSize(int expected) {
