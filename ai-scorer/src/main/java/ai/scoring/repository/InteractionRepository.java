3b532891873117d4214a9bcfe086c69cd977ca19
Eric Deandrea <eric.deandrea@gmail.com>
2026-01-28 21:10:22 -0500

    Work for JFokus

--- ai-scorer/src/main/java/ai/scoring/repository/InteractionRepository.java
+++ ai-scorer/src/main/java/ai/scoring/repository/InteractionRepository.java
@@ -1,9 +1,12 @@
 package ai.scoring.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.transaction.Transactional;
 
 import ai.scoring.domain.interaction.Interaction;
@@ -16,6 +19,12 @@ import io.quarkus.panache.common.Parameters;
 
 @ApplicationScoped
 public class InteractionRepository implements PanacheRepositoryBase<Interaction, UUID> {
+	private final CriteriaBuilder criteriaBuilder;
+
+	public InteractionRepository(CriteriaBuilder criteriaBuilder) {
+		this.criteriaBuilder = criteriaBuilder;
+	}
+
 	@Transactional
 	public boolean containsSource(Source source) {
 		Log.debugf("Checking if source %s exists", source);
@@ -30,7 +39,33 @@ public class InteractionRepository implements PanacheRepositoryBase<Interaction,
 
 	@Transactional
 	public List<Interaction> findInteractions(InteractionQuery query) {
-		return null;
+		var q = this.criteriaBuilder.createQuery(Interaction.class);
+		var root = q.from(Interaction.class);
+		var predicates = new ArrayList<Predicate>();
+
+		query.getApplicationName()
+			.map(applicationName -> this.criteriaBuilder.equal(root.get("applicationName"), applicationName))
+			.ifPresent(predicates::add);
+
+		query.getInterfaceName()
+			.map(interfaceName -> this.criteriaBuilder.equal(root.get("interfaceName"), interfaceName))
+			.ifPresent(predicates::add);
+
+		query.getMethodName()
+			.map(methodName -> this.criteriaBuilder.equal(root.get("methodName"), methodName))
+			.ifPresent(predicates::add);
+
+		query.getStart()
+			.map(start -> this.criteriaBuilder.greaterThanOrEqualTo(root.get("interactionDate"), start))
+			.ifPresent(predicates::add);
+
+		query.getEnd()
+			.map(end -> this.criteriaBuilder.lessThanOrEqualTo(root.get("interactionDate"), end))
+			.ifPresent(predicates::add);
+
+		q.where(predicates.toArray(Predicate[]::new));
+
+		return getEntityManager().createQuery(q).getResultList();
 	}
 
 	private static Parameters parameters(Source source) {
